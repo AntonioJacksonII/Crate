e759ba62fa1800ad4e0b895fcd818cb1dd9fd044
Jack Cullen <jpcullen20@gmail.com>
2020-08-22 13:41:32 -0600

    Annotate user migration

--- code/api/src/migrations/1-user.js
+++ code/api/src/migrations/1-user.js
@@ -1,5 +1,6 @@
 // This looks very similar to a migration in Rails and it seems like
 // we will want to do another one for a survey resource that belongs to a user
+// or we will have to add another field to the user table to store the stylen they were givin
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -9,6 +10,7 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+        // I think Sequelize just translates to SQL 
       },
       name: {
         type: Sequelize.STRING
